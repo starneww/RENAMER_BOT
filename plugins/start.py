c95760d8f61b62cda2bc4d1a0a4886508092b07d
⭐star⭐ <107930379+starneww@users.noreply.github.com>
2022-06-22 19:04:11 +0530

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,36 @@
+from pyrogram import Client, filters
+from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+import humanize
+from helper.database import  insert 
+
+@Client.on_message(filters.private & filters.command(["start"]))
+async def start(client,message):
+	insert(int(message.chat.id))
+	await message.reply_text(text =f"""
+	Hello {message.from_user.first_name }
+	__➠ I ᴀᴍ ᴀ Sɪᴍᴘʟᴇ TG Fɪʟᴇ 📁 ᴀᴜᴅɪᴏ/ᴍᴘ3 🎶 & ᴠɪᴅᴇᴏ 🎥 Rᴇɴᴀᴍᴇʀ ʙᴏᴛ ᴡɪᴛʜ ᴘᴇʀᴍᴀɴᴇɴᴛ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ sᴜᴘᴘᴏʀᴛ 🎞\n\n➠Sɪᴍᴘʟʏ Sᴇɴᴅ ᴀɴʏ Iᴍᴀɢᴇ 📷 ᴛᴏ sᴇᴛ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ 🔳 
+	__\n\n➠**Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ**: @robo_glitch__
+	""",reply_to_message_id = message.message_id ,  
+	reply_markup=InlineKeyboardMarkup([[
+          InlineKeyboardButton("📮 Sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ 📮" ,url="https://t.me/dubbedweb"), 
+	  InlineKeyboardButton("🔮 ᴏᴛʜᴇʀ ʙᴏᴛs 🔮", url="https://t.me/futurebackups")
+          ],[
+          InlineKeyboardButton("📣  ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟ ⚡", url="https://t.me/hddubhub4u")
+          ]]
+          )
+        )
+
+
+
+@Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
+async def send_doc(client,message):
+       media = await client.get_messages(message.chat.id,message.message_id)
+       file = media.document or media.video or media.audio 
+       filename = file.file_name
+       filesize = humanize.naturalsize(file.file_size)
+       fileid = file.file_id
+       await message.reply_text(
+       f"""__ᴡʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴍᴇ ᴛᴏ ᴅᴏ ᴡɪᴛʜ ᴛʜɪs ꜰɪʟᴇ❓__\n {filename} ""
+       ,reply_to_message_id = message.message_id,
+       reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename ",callback_data = "rename")
+       ,InlineKeyboardButton("Cancel ✖️",callback_data = "cancel")  ]]))
